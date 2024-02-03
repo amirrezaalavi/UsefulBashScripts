f5f3a4529a168136c3641724eb17ea93ed326ae3
amiralavi-Thinkpad <alavi2004@outlook.com>
2024-02-03 23:01:43 +0330

    fixing bunch of bugs and restarting network service after dns change

--- changedns.sh
+++ changedns.sh
@@ -8,12 +8,15 @@ if [[ "$input" == "1" ]]; then
     echo "Changing DNS..."
     echo "nameserver 178.22.122.100" | sudo tee /etc/resolv.conf > /dev/null
     echo "nameserver 185.51.200.2" | sudo tee /etc/resolv.conf > /dev/null
-elif [[ "$input" == "2"]]; then
+    sudo systemctl restart systemd-resolved
+elif [[ "$input" == "2" ]]; then
     echo "nameserver 178.22.122.100" | sudo tee /etc/resolv.conf > /dev/null
     echo "nameserver 178.22.122.100" | sudo tee /etc/resolv.conf > /dev/null
+    sudo systemctl restart systemd-resolved
 elif [[ "$input" == "3" ]]; then
     echo "Removing DNS..."
     echo "" | sudo tee /etc/resolv.conf > /dev/null
+    sudo systemctl restart systemd-resolved
 else
     echo "Please Enter 1 or 2 or 3"
 fi
